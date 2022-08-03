77875c97632e3f0775f21fef4a922da972e50924
Tasneem Titus <titustasneem5@gmail.com>
2022-08-03 15:33:59 +0200

    used ternary operator

--- src/index.js
+++ src/index.js
@@ -1,42 +1,59 @@
-const card = document.querySelectorAll('.memory-card');
+const card = document.querySelectorAll(".memory-card");
 
 let hasFlippedCard = false;
 let firstCard, secondCard;
 
-function flipCard(){
-    this.classList.toggle('flip')
+function flipCard() {
+  this.classList.toggle("flip");
+
+  if (!hasFlippedCard) {
     // first click
-    if(!hasFlippedCard) {
-        hasFlippedCard = true;
-        firstCard = this;
-            // console.log(hasFlippedCard, firstCard);   
-    } else {
-        // second click
-        hasFlippedCard = false;
-        secondCard = this;
-        console.log(firstCard, secondCard);
-    }
+    hasFlippedCard = true;
+    firstCard = this;
+    // console.log(hasFlippedCard, firstCard);
 
-    // check if cards match?
-    // console.log(firstCard.dataset.name)
-    // console.log(secondCard.dataset.name)
-    if(firstCard.dataset.name === secondCard.dataset.name) {
-        // its a match (disable click )
-        firstCard.removeEventListener('click', flipCard);
-        secondCard.removeEventListener('click', flipCard);
-    } else{
-        setTimeout(()=> {
-        firstCard.classList.remove('flip');
-        secondCard.classList.remove('flip')
-        }, 1500)
-        
+    return;
+  } 
+    // second click
+    hasFlippedCard = false;
+    secondCard = this;
+    // console.log(firstCard, secondCard);
+    getMatchingCards();
+  
+}
+// console.log('function was executed')
 
-    }
-    // console.log('function was executed')
+// console.log('I was clicked!');
+// console.log(this);
 
+// function getMatchingCards() {
+//   // check if cards match?
+//   // console.log(firstCard.dataset.name)
+//   // console.log(secondCard.dataset.name)
+//   if (firstCard.dataset.name === secondCard.dataset.name) {
+//     // its a match (disable click )
+//     disableCardClick();
+//   } else {
+//    unflippedCards();
+//   }
+// }
 
-    // console.log('I was clicked!');
-    // console.log(this);
+function getMatchingCards(){
+    let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+
+    isMatch ? disableCardClick() : unflippedCards();
 }
 
-card.forEach(card => card.addEventListener('click', flipCard))
+function disableCardClick(){
+ firstCard.removeEventListener("click", flipCard);
+ secondCard.removeEventListener("click", flipCard);
+}
+
+function unflippedCards(){
+    setTimeout(() => {
+        firstCard.classList.remove("flip");
+        secondCard.classList.remove("flip");
+      }, 1000);
+}
+
+card.forEach((card) => card.addEventListener("click", flipCard));
