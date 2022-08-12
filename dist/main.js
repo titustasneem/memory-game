7177f93fdfcb16191d80741e10bf82e5d8fb6a35
Tasneem Titus <titustasneem5@gmail.com>
2022-08-12 08:40:50 +0200

    changed 100vh to 100% css

--- dist/main.js
+++ dist/main.js
@@ -13,20 +13,44 @@
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ (() => {
+/***/ ((module) => {
 
-eval("// // make memoryCards flip\r\nconst memoryCards = document.querySelectorAll(\".memory-card\");\r\n\r\n// When player clicks card, we have to know if it is the first or second card\r\nlet flippedCards = false;\r\nlet cardOne, cardTwo;\r\n\r\n// lock board to wait for memoryCards to flip finish \r\nlet lockBoard = false;\r\n\r\n\r\n\r\n \r\nfunction flipMemoryCard() {\r\n\r\n  console.log('I was clicked')\r\n   // this represents the element that fired the event \r\n  console.log(this)\r\n\r\n  if (lockBoard) return;\r\n  \r\n  // don't click on first card twice and disaBLE CARD CLICK\r\n   if (this === cardOne) return;\r\n\r\n   // access the classList of the memory card and toggle the flip class\r\n   // toggle means that if the class is there then remove it, if not then add it\r\n  this.classList.add(\"flip\");\r\n \r\n  // if has flipped card is false then it is the first time a player has clicked a card \r\n  if (!flippedCards) {\r\n   //   // first click\r\n     flippedCards = true;\r\n     cardOne = this;\r\n \r\n    return;\r\n  }\r\n    // second click\r\n   flippedCards = false;\r\n   cardTwo = this;\r\n  matchingMemoryCards();\r\n}\r\n\r\n\r\n \r\nfunction matchingMemoryCards() {\r\n  let isCardsMatching = cardOne.dataset.name === cardTwo.dataset.name;\r\n \r\n  isCardsMatching ? disableCardClick() : unflippedmemoryCards();\r\n}\r\n \r\nfunction disableCardClick() {\r\n  cardOne.removeEventListener(\"click\", flipMemoryCard);\r\n  cardTwo.removeEventListener(\"click\", flipMemoryCard);\r\n  resetBoard();\r\n}\r\n\r\n\r\n// if memoryCards is not a match, we won't flip them\r\n// remove flip class from card\r\nfunction unflippedmemoryCards() {\r\n  // only lock board if memoryCards have been flipped\r\n  lockBoard = true;\r\n \r\n  setTimeout(() => {\r\n    cardOne.classList.remove(\"flip\");\r\n    cardTwo.classList.remove(\"flip\");\r\n    lockBoard = false;\r\n    resetBoard();\r\n  }, 1000);\r\n}\r\n\r\nfunction resetBoard() {\r\n  [flippedCards, lockBoard] = [false, false];\r\n  [cardOne, cardTwo] = [null, null];\r\n}\r\n\r\n\r\n\r\n\r\n // should loop through all the memoryCards and add an event listener\r\n // listen for a click event\r\n // when that event is fired then flipMemoryCard function will be executed\r\n memoryCards.forEach((card) => card.addEventListener(\"click\", flipMemoryCard));\r\n\r\n\r\n (function shuffleMemoryCards() {\r\n  cards.forEach((card) => {\r\n    let randomCardPosition = Math.floor(Math.random() * 12);\r\n    card.style.order = randomCardPosition;\r\n  });\r\n})();\r\n \r\ncards.forEach((card) => card.addEventListener(\"click\", flipCard));\r\n \r\nfunction countdownTimer() {\r\n  const timer = document.getElementById('timer');\r\n  let seconds = 30;\r\n  const interval = setInterval(() => {\r\n    seconds--;\r\n    timer.innerHTML = `You have ${seconds} seconds left to match all cards`;\r\n    if (seconds < 1) {\r\n      clearInterval(interval);\r\n      alert(\"You have run out of time. Try again...\");\r\n      setTimeout(() => location.reload(), 2000);\r\n    }\r\n  }, 1000);\r\n}\r\ncountdownTimer()\r\n \r\n\r\n\n\n//# sourceURL=webpack://tasneem-titus-222-memory-game-in-vanilla-js-javascript/./src/index.js?");
+eval("const memoryCards = document.querySelectorAll(\".memory-card\");\r\n\r\nlet matchCounter = 0;\r\nlet flippedCards = false;\r\nlet lockBoard = false;\r\nlet cardOne, cardTwo;\r\n\r\n(function beginShuffle() {\r\n  shuffleMemoryCards();\r\n})();\r\n\r\nfunction matchMemoryCards(firstCard, secondCard) {\r\n  let foundMatch = firstCard === secondCard;\r\n\r\n  if (foundMatch) {\r\n    matchCounter++;\r\n    disableCardClick();\r\n  } else {\r\n    unflipCards();\r\n  }\r\n\r\n  if (matchCounter == memoryCards.length / 2) {\r\n    window.alert(\"Congratulations! You Won!\");\r\n    setTimeout(() => location.reload(), 2000);\r\n  }\r\n}\r\n\r\nfunction flipMemoryCard() {\r\n  if (lockBoard) {\r\n    return;\r\n  }\r\n\r\n  if (this === cardOne) {\r\n    return;\r\n  }\r\n\r\n  this.classList.add(\"flip\");\r\n\r\n  if (!flippedCards) {\r\n    flippedCards = true;\r\n    cardOne = this;\r\n    return;\r\n  } else {\r\n    flippedCards = false;\r\n    cardTwo = this;\r\n  }\r\n  matchMemoryCards(cardOne.dataset.name, cardTwo.dataset.name);\r\n}\r\n\r\nfunction resetBoard() {\r\n  [flippedCards, lockBoard] = [false, false];\r\n  [cardOne, cardTwo] = [null, null];\r\n}\r\n\r\nfunction disableCardClick() {\r\n  cardOne.removeEventListener(\"click\", flipMemoryCard);\r\n  cardTwo.removeEventListener(\"click\", flipMemoryCard);\r\n\r\n  resetBoard();\r\n}\r\n\r\nfunction unflipCards() {\r\n  lockBoard = true;\r\n\r\n  setTimeout(() => {\r\n    cardOne.classList.remove(\"flip\");\r\n    cardTwo.classList.remove(\"flip\");\r\n    lockBoard = false;\r\n    resetBoard();\r\n  }, 1000);\r\n  return true;\r\n}\r\n\r\nmemoryCards.forEach((card) => card.addEventListener(\"click\", flipMemoryCard));\r\n\r\nfunction shuffleMemoryCards() {\r\n  memoryCards.forEach((card) => {\r\n    let randomPosition = Math.floor(Math.random() * 12);\r\n    card.style.order = randomPosition;\r\n  });\r\n}\r\n\r\nmodule.exports = {\r\n  memoryCards,\r\n  matchMemoryCards,\r\n  flippedCards,\r\n  disableCardClick,\r\n  unflipCards,\r\n  resetBoard,\r\n  shuffleMemoryCards,\r\n};\n\n//# sourceURL=webpack://tasneem-titus-222-memory-game-in-vanilla-js-javascript/./src/index.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
 /******/ 	
 /******/ })()
 ;
