36daa577bea4a5eb00c93316940f3f400f940110
Tasneem Titus <titustasneem5@gmail.com>
2022-09-12 12:28:18 +0200

    added final spec

--- spec/index_spec.js
+++ spec/index_spec.js
@@ -1,66 +1,3 @@
-// const jsdom = require("jsdom");
-// const fs = require("fs");
-// const { JSDOM } = jsdom;
-// const index = fs.readFileSync("index.html", "utf-8");
-// const { document } = new JSDOM(index).window;
-// global.document = document;
-
-// const game = require("../src/index");
-
-// describe("memoryCards", function () {
-//   it("should check if memoryCards is defined", function () {
-//     expect(game.memoryCards).toBeDefined();
-//   });
-//   it("should add an eventListener to every card", function () {
-//     spyOn(game, "memoryCards");
-//     expect(game.memoryCards).toBeDefined();
-//   });
-// });
-
-// describe("flippedCards", function () {
-//   it("should check flip the card onto the front-face when clicked on", function () {
-//     expect(game.flippedCards).toBeDefined();
-//   });
-// });
-
-// describe("disableCardClick", function () {
-//   it("should disable the card click", function () {
-//     spyOn(game, "disableCardClick");
-//     game.disableCardClick();
-//     expect(game.disableCardClick).toHaveBeenCalled();
-//   });
-// });
-
-// describe("resetBoard", function () {
-//   it("should reset the board", function () {
-//     spyOn(game, "resetBoard");
-//     game.resetBoard();
-//     expect(game.resetBoard).toHaveBeenCalled();
-//   });
-// });
-
-// describe("shuffleMemoryCards", function () {
-//   it("should shuffle the memory cards", function () {
-//     spyOn(game, "shuffleMemoryCards");
-//     game.shuffleMemoryCards();
-//     expect(game.shuffleMemoryCards).toHaveBeenCalled();
-//   });
-// });
-
-// describe("unflipCards", function () {
-//   it("should unflip the cards", function () {
-//     const card1 = document.getElementsByClassName("memory-card")[0];
-//     const card2 = document.getElementsByClassName("memory-card")[2];
-//     spyOn(game, "unflipCards");
-//     game.unflipCards();
-//     game.matchMemoryCards(card1.dataset.name, card2.dataset.name);
-//     expect(game.unflipCards).toHaveBeenCalled();
-//   });
-// });
-
-
-// -------------------------------------------------------------------------------------------------------
-
 const jsdom = require("jsdom");
 const fs = require("fs");
 const { JSDOM } = jsdom;
@@ -69,7 +6,6 @@ const { document } = new JSDOM(index).window;
 global.document = document;
 
 const game = require("../src/index");
-
 const gameCharacterArray = [
   "Johnny",
   "kano",
@@ -79,43 +15,57 @@ const gameCharacterArray = [
   "Sonya",
 ];
 
-let gameCharacters;
-
-describe('gameCharacterArray', () => {
-  beforeEach(() => {
-    gameCharacters = [...gameCharacterArray]
-    // console.log(gameCharacters)
+describe("disableCardClick", function () {
+  it("should disable the card click", function () {
+    spyOn(game, "disableCardClick");
+    game.disableCardClick();
+    expect(game.disableCardClick).toHaveBeenCalled();
   });
-  // console.log(gameCharacters)
+});
 
-  it('should contain an array of al the game characters', () => {
+describe("resetBoard", function () {
+  it("should reset the board", function () {
+    spyOn(game, "resetBoard");
+    game.resetBoard();
+    expect(game.resetBoard).toHaveBeenCalled();
+  });
+});
+
+describe("unflipCards", function () {
+  it("should unflip the cards", function () {
+    spyOn(game, "unflipCards");
+    game.unflipCards();
+    expect(game.unflipCards).toHaveBeenCalled();
+  });
+});
+
+describe("gameCharacterArray", () => {
+  beforeEach(() => {
+    gameCharacters = [...gameCharacterArray];
+  });
+
+  it("should contain an array of all the game characters", () => {
     expect(gameCharacters.length).toBe(6);
   });
 });
 
-  describe('gameCharactersArray', () => {
-    it('should be shuffled in any random order', () => {
-      expect([...gameCharacterArray]) 
-      .toMatch(/Johnny|kano|Kitana|Raiden| Scorpion|Sonya/);
-      // expect(memoryGame.cards instanceof Array).toBe(true);
-    });
+describe("gameCharactersArray", () => {
+  it("should be shuffled in any random order", () => {
+    expect([...gameCharacterArray]).toMatch(
+      /Johnny|kano|Kitana|Raiden| Scorpion|Sonya/
+    );
   });
-  describe('flipMemoryCard function', () => {
-    // beforeEach(() => {
-    //    const memoryCards = document.querySelector(`.memory-card${num}`);
-    // });
- it('should add an event listener to the cards and enable them to flip', () => {
-      expect(game.flipMemoryCard).toBeDefined();
-    });
+});
+describe("flipMemoryCard function", () => {
+  it("should add an event listener to the cards and enable them to flip", () => {
+    expect(game.flipMemoryCard).toBeDefined();
   });
+});
 
-   
-  describe('init', function() {
-
-    it('should create a 2x2 grid when the displayCardsDiv function is called', function() {
-      spyOn(game, 'displayCardsDiv');
-      game.displayCardsDiv();
-    // let gameLayout = document.querySelector("#grid").value = 4;
-      expect(game.displayCardsDiv).toHaveBeenCalled();
-    });
+describe("displayCardsDiv", function () {
+  it("should create a 2x2 grid when the displayCardsDiv function is called", function () {
+    spyOn(game, "displayCardsDiv");
+    game.displayCardsDiv();
+    expect(game.displayCardsDiv).toHaveBeenCalled();
   });
+});
--- src/index.js
+++ src/index.js
@@ -1,229 +1,3 @@
-// const gameCharacterArray = [
-//   "Johnny",
-//   // "Johnny",
-//   "kano",
-//   // "kano",
-//   // "Kitana",
-//   "Kitana",
-//   // "Raiden",
-//   "Raiden",
-//   // "Scorpion",
-//   "Scorpion",
-//   // "Sonya",
-//   "Sonya",
-// ];
-
-// for (let i = 0; i < gameCharacterArray.length; i++) {
-//   let j = Math.floor(Math.random() * gameCharacterArray.length);
-//   let temp = gameCharacterArray[i];
-//   gameCharacterArray[i] = gameCharacterArray[j];
-//   gameCharacterArray[j] = temp;
-// }
-
-// let randomGameCharacter = [];
-
-// let matchCounter = 0;
-// let isCardFlipped = false;
-// let lockBoard = false;
-// let cardOne, cardTwo;
-
-// // --------------------------------------------------------------------------------------
-
-// // (function beginShuffle() {
-// //   shuffleMemoryCards();
-// // })();
-
-// // ---------------------------------------------------------------------------------------
-
-// // function shuffleMemoryCards() {
-// //   memoryCards.forEach((card) => {
-// //     let randomPosition = Math.floor(Math.random() * 12);
-// //     card.style.order = randomPosition;
-// //   });
-// // }
-
-// // --------------------------------------------------------------------------------------
-
-// function flipMemoryCard(num) {
-
-//   // console.log(num);
-//   const memoryCards = document.querySelector(`.memory-card${num}`);
-//   // memoryCards.classList
-//   // console.log(memoryCards);
-//   if (lockBoard) {
-//     return;
-//   }
-
-//   if (memoryCards === cardOne) {
-//     return;
-//   }
-//   // console.log(memoryCards)
-
-//   memoryCards.classList.add("flip");
-
-//   if (!isCardFlipped) {
-//     isCardFlipped = true;
-//     cardOne = memoryCards;
-//     return;
-//   } else {
-//     lockBoard = true;
-//     cardTwo = memoryCards;
-//   }
-
-//   matchMemoryCards(cardOne.dataset.name, cardTwo.dataset.name);
-// }
-
-// // -----------------------------------------------------------------------
-// function matchMemoryCards(firstCard, secondCard) {
-//   let foundMatch = firstCard === secondCard;
-
-//   if (foundMatch) {
-//     countMatches();
-//     disableCardClick();
-//   } else {
-//     unflipCards();
-//   }
-// }
-// // ---------------------------------------------------------------------------
-
-// function countMatches() {
-//   matchCounter = matchCounter + 1;
-//   if (foundAllMatches()) {
-//     setTimeout(function () {
-//       alert("Congratulations! You won!");
-//       setTimeout(() => {
-//         location.reload(), 2000;
-//       });
-//     }, 1000);
-//   }
-// }
-
-// // --------------------------------------------------------------------------
-
-// function foundAllMatches() {
-//   if (matchCounter == randomGameCharacter.length / 2) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
-
-// // ----------------------------------------------------------------------------
-
-// function disableCardClick() {
-//   cardOne.removeEventListener("click", flipMemoryCard);
-//   cardTwo.removeEventListener("click", flipMemoryCard);
-
-//   resetBoard();
-// }
-
-// // ----------------------------------------------------------------------------------
-
-// function resetBoard() {
-//   [isCardFlipped, lockBoard] = [false, false];
-//   [cardOne, cardTwo] = [null, null];
-// }
-
-// // -------------------------------------------------------------------------------------
-
-// function unflipCards() {
-//   lockBoard = true;
-
-//   setTimeout(() => {
-//     cardOne.classList.remove("flip");
-//     cardTwo.classList.remove("flip");
-//     lockBoard = false;
-//     resetBoard();
-//   }, 1000);
-//   return true;
-// }
-
-// // ---------------------------------------------------------------------------------------
-
-// memoryCards.forEach((card) => card.addEventListener("click", flipMemoryCard));
-
-// // ------------------------------------------------------------------------------------------
-
-// // let memoryGameContainer = document.querySelector(".memory-game");
-
-// function displayCardsDiv() {
-//   let value = document.querySelector("#grid").value;
-//   // console.log(value);
-//   let num = 0;
-
-//   const div = document.querySelector(".tester");
-
-//   if (value === "") {
-//     return false;
-//   }
-
-//   if (value === "first option") {
-//     num = 4;
-//     div.style.gridTemplateColumns = "repeat(2, auto)";
-//   } else if (value === "second option") {
-//     num = 6;
-//     div.style.gridTemplateColumns = "repeat(3, auto)";
-//   } else if (value === "third option") {
-//     num = 12;
-//     div.style.gridTemplateColumns = "repeat(4, auto)";
-//   }
-
-//   let p = document.querySelector(".tester");
-//   // console.log(p.innerHTML);
-//   p.innerHTML = "";
-
-//    randomGameCharacter = [...gameCharacterArray.slice(0,num / 2) , ...gameCharacterArray.slice(0, num / 2) ];
-//   // console.log(testing)
-
-//   // randomGameCharacter = gameCharacterArray.slice(0, num / 2);
-
-//   for (let i = 0; i < randomGameCharacter.length; i++) {
-//     let j = Math.floor(Math.random() * randomGameCharacter.length);
-//     let temp = randomGameCharacter[i];
-//     randomGameCharacter[i] = randomGameCharacter[j];
-//     randomGameCharacter[j] = temp;
-//   }
-
-//   for (let i = 0; i < num; i++) {
-//     p.innerHTML += `<div class="memory-card${i}" data-name="${randomGameCharacter[i]}" onclick='flipMemoryCard(${i})' >
-//     <img class="front-face" src="./src/${randomGameCharacter[i]}.png"/>
-//     <img class="back-face" src="./src/mk5.png" alt="Mortal Combat" />
-//   </div>`;
-//   }
-// }
-// displayCardsDiv();
-
-// // --------------------------------------------------------------------------------------------------------------------------
-// module.exports = {
-//   memoryCards,
-//   matchMemoryCards,
-//   isCardFlipped,
-//   disableCardClick,
-//   unflipCards,
-//   resetBoard,
-//   shuffleMemoryCards,
-// };
-
-// v
-// v
-// v
-// v
-// v
-// v
-// v
-// v
-// v
-// v
-// v
-// v
-// v
-// v
-// v
-// v
-// v
-// v
-// v
-
 const gameCharacterArray = [
   "Johnny",
   "kano",
@@ -247,7 +21,6 @@ let isCardFlipped = false;
 let lockBoard = false;
 let cardOne, cardTwo;
 
-
 function flipMemoryCard(num) {
   const memoryCards = document.querySelector(`.memory-card${num}`);
 
@@ -267,7 +40,6 @@ function flipMemoryCard(num) {
 
   matchMemoryCards(cardOne.dataset.name, cardTwo.dataset.name);
   memoryCards.forEach((card) => card.addEventListener("click", flipMemoryCard));
-
 }
 
 function matchMemoryCards(firstCard, secondCard) {
@@ -369,8 +141,7 @@ function displayCardsDiv() {
 }
 displayCardsDiv();
 
-module.exports ={
-  
+module.exports = {
   matchMemoryCards,
   isCardFlipped,
   disableCardClick,
